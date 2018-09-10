0f44ea8566d39f9467f52cafc2b015dbf1b03e9f
Jazmin Cardona <jazmincardonameza@gmail.com>
2018-09-09 19:24:24 -0500

    JACM07240909

---
+++ src/main/java/util/SearchToken.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import antlr.Token;
+import dao.GenericDao;
+import java.util.List;
+
+/**
+ *
+ * @author Ismael
+ */
+public class SearchToken {
+    
+    public SearchToken() {}
+
+    private boolean search(String token) {
+        GenericDao dao = new GenericDao();
+        List<Token> list = dao.getByParameter(Token.class.getSimpleName(), "token", token);
+        return (list.isEmpty()) ? false:true;
+    }
+    
+  
+    
+}
